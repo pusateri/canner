8ae364f27352d54b27d0754436be5015e20b82b0
Keith Hubbard <keith@bangj.com>
2008-03-11 11:12:23 -0400

    Timeout fix: use explicit default timeout value

--- src/canner/Session.py
+++ src/canner/Session.py
@@ -94,7 +94,7 @@ class Session(object):
 
         self.personality = None
         self.child = None
-        self.timeout = -1
+        self.timeout = 30
 
         self.logfile = open("pexpect.log", "w") if shouldLog else None
 
