be0a838e245e96647ae9ba595626d51716dad033
Tom Pusateri <pusateri@bangj.com>
2008-04-01 10:09:59 -0400

    Add beginning support for Juniper netscreen ScreenOS firewall devices.

---
+++ canner/personalities/netscreen.py
@@ -0,0 +1,29 @@
+#
+# Copyright 2008 !j Incorporated
+#
+# This file is part of Canner.
+#
+# Canner is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Canner is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Canner.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from . import Personality
+import re
+
+class NetscreenPersonality(Personality):
+    
+    os_name = "ScreenOS"
+            
+    @classmethod
+    def match(cls, info):
+        return re.search(r"Remote Management Console", info)
--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(
             "ios = canner.personalities.ios:IOSPersonality",
             "ios_xr = canner.personalities.ios_xr:IOSXRPersonality",
             "junos = canner.personalities.junos:JUNOSPersonality",
+            "netscreen = canner.personalities.netscreen:NetscreenPersonality",
             "procket = canner.personalities.procket:ProcketPersonality",
             "smc = canner.personalities.smc:SMCPersonality",
             ],
